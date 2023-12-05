9d003a55d0c0eee8d461c5697596919f43b65298
Jona Ohme <ohmejona@gmail.com>
2023-12-05 03:05:34 +0100

    add <space>pv ...

--- init.lua
+++ init.lua
@@ -262,6 +262,7 @@ vim.o.completeopt = 'menuone,noselect'
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
